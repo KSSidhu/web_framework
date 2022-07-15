d790779a3fb9d84041019f4e9913b19cedd99671
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2022-07-15 15:59:45 -0700

    add user class starting point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hi there!");
+import { User } from "./models/User";
+
+const user = new User({ name: "Kirat", age: 24 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
+
+user.set({ name: "newName", age: 99 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
