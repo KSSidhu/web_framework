c33f0f9a77afc99e6c04fe4d78f65bdf126c0050
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2022-07-15 17:13:47 -0700

    Add basic save example

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "Kirat", age: 24 });
+const user = new User({ name: "new User", age: 0 });
 
-console.log(user.get("name"));
-console.log(user.get("age"));
-
-user.on("change", () => {
-	console.log("Change #1");
-});
-user.on("change", () => {
-	console.log("Change #1");
-});
-user.on("save", () => {
-	console.log("Save was triggered");
-});
-
-user.trigger("change");
+//user.save();
