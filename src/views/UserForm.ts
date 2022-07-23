6ad34f18fe8146f10ad6a0e6a51c6c3f56301e61
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2022-07-23 12:37:19 -0700

    Add basic form view

---
+++ src/views/UserForm.ts
@@ -0,0 +1,18 @@
+export class UserForm {
+	constructor(public parent: Element) {}
+
+	template(): string {
+		return `
+		<div>
+			<h1>User Form</div>
+			<input /> 
+		</div>
+		`;
+	}
+
+	render(): void {
+		const templateElement = document.createElement("template");
+		templateElement.innerHTML = this.template();
+		this.parent.append(templateElement.content);
+	}
+}
